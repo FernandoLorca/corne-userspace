035d4de2574a07e36e41d39d395d5814b60188c1
FernandoLorca <florcapinto@gmail.com>
2024-08-29 07:47:03 -0400

    update

--- FernandoLorca/keymap.c
+++ FernandoLorca/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       TD(TD_LSHIFT_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  XXXXXXX,
+      KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_LALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL,   MO(_LOWER),  KC_SPC,     KC_ENT,   MO(_RISE), KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -75,11 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *                         | LCTRL |LOWER | /Space  /       \Enter \  |HIGHER| LAlt |
     *                          `---------------------'           '------''-------------'
     */
-
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+      KC_LCTL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_LALT, XXXXXXX, KC_MPLY, KC_VOLU, KC_LEFT_GUI,                  KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_PRINT_SCREEN, KC_DEL,
+      KC_LSFT, XXXXXXX, KC_LALT, KC_MPLY, KC_VOLU, KC_LEFT_GUI,                  KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_PRINT_SCREEN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT,                       KC_HOME, KC_PGDN, KC_PGUP, KC_END, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
